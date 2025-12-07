bdbb1c360f02a7f1ec0712a0a0cbdc076078e542
Rafael Carvalho <rafaelngcarvalho@hotmail.com>
2025-12-07 02:18:28 -0300

    commit inicial

---
+++ src/model/entities/Account.java
@@ -0,0 +1,71 @@
+package model.entities;
+
+import model.exceptions.DomainException;
+
+public class Account {
+	
+	private Integer number;
+	private String holder;
+	private Double balance;
+	private Double withdrawLimit;
+	
+	public Account() {
+		
+	}
+	
+	public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withdrawLimit = withdrawLimit;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getWithdrawLimit() {
+		return withdrawLimit;
+	}
+
+	public void setWithdrawLimit(Double withdrawLimit) {
+		this.withdrawLimit = withdrawLimit;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+	
+	public void deposit(Double amount) {
+		this.balance += amount;
+	}
+	
+	public void withdraw(Double amount) {
+		validateWithdraw(amount);
+		this.balance -= amount; 
+	}
+	
+	private void validateWithdraw(double amount) {
+		if(amount > getWithdrawLimit()) {
+			throw new DomainException("Withdraw error: The amount exceeds withdraw limit.");
+		}
+		if (amount > getBalance()) {
+			throw new DomainException("Withdraw error: Not enough balance.");
+		} 
+	}
+	
+	@Override
+	public String toString() {
+		return "Account [number=" + number + ", holder=" + holder + ", balance=" + balance + ", withdrawLimit="
+				+ withdrawLimit + "]";
+	}
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module exercicio_exceptions_2 {
+}
